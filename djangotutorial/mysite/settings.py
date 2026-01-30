21fa503a7670e9fbf1b6c79f58803db34a10c4dd
PinkeshVasava <pinkeshvsv3557@gmail.com>
2026-01-30 11:47:41 +0530

    Add initial polls app setup with models and migrations

--- djangotutorial/mysite/settings.py
+++ djangotutorial/mysite/settings.py
@@ -28,14 +28,14 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
-# Application definition
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "polls.apps.PollsConfig",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
 
 MIDDLEWARE = [
